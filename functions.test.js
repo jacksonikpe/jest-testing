c5255a50b5a638d588ed66cf996438852dc778a1
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-02-26 09:29:40 +0100

    null test

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,17 @@ const functions = require("./functions");
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Adds 2 + 2 to not equal 4", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+//check for truthy and falsy values
+//toBeNull matches only null
+// toBeUndifined matches only undefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statment treats as false
+
+test("should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
