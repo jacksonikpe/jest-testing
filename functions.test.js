98a0f3bcb534b5467172a7076a14af20b94a7617
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-03-19 13:32:50 +0100

    test update

--- functions.test.js
+++ functions.test.js
@@ -28,3 +28,38 @@ test("User should be Jackson Collins-Ikpe", () => {
     lastName: "Collins-Ikpe",
   });
 });
+
+//less than and greater than
+test("should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regex
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+//Arrays
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
+
+//working with async data
+
+//promise
+// test("user fetched name should be Leanne Graham", () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual("Leanne Graham");
+//   });
+// });
+
+//Async Await
+test("user fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
+});
